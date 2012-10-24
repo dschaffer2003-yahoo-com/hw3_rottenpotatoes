cf177e17b464b072c6ab3ad923d3355a91601cad
Dan <dschaffer2003@yahoo.com>
2012-10-24 04:45:57 +0000

    3.2 done

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,10 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(:title => movie["title"],
+        :release_date => movie["release_date"],
+        :rating => movie["rating"])
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -16,12 +18,44 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  page.content  is the entire content of the page as a string.
   flunk "Unimplemented"
 end
+Then /I should see the appropriate movies/ do
+  Movie.find_all_by_rating(["PG-13", "R"]).each do |x|
+    str = 'I should see "' + x.title + '"'
+    step str
+  end
+  Movie.find_all_by_rating(["G", "PG"]).each do |x|
+    str = 'I should not see "' + x.title + '"'
+    step str
+  end
+
+#  Movie.find_all_by_rating(["PG-13", "R"]).each do |x|
+#  assert page.has_content?(x.title)
+#  end
+end
+
+Then /I should see no movies/ do
+  Movie.find_all_by_rating(["G", "PG", "PG-13", "R"]).each do |x|
+    str = 'I should not see "' + x.title + '"'
+    step str
+  end
+end
+
+Then /I should see all movies/ do
+  Movie.find_all_by_rating(["G", "PG", "PG-13", "R"]).each do |x|
+    str = 'I should see "' + x.title + '"'
+    step str
+  end
+end
+
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(/, */).each do |rating|
+    step "I #{uncheck}check \"ratings_#{rating}\""
+  end
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
